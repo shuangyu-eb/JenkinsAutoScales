fe5f47dbe50660b0049148d148f728b644e88fc7
zsy <shuangyuzhu0069@gmail.com>
2020-10-27 17:52:32 +0800

    add/remove docker cloud info function added

--- yamlUtils.py
+++ yamlUtils.py
@@ -1,6 +1,7 @@
 import yaml
 import os
 import re
+import bios
 
 
 ## 读取yaml文件
@@ -44,7 +45,7 @@ def collect_docker_cloud_ips(file_name):
     yaml_infos = read_yaml(jenkins_configuration_path)
     docker_cloud_ips = []
     for i in range(len(yaml_infos['jenkins']['clouds'])):
-        str = "tcp://54.199.31.91:4243"
+        # str = "tcp://54.199.31.91:4243"
         # print(re.findall(r"tcp://(.+?):4243", str))
         # print(yaml_infos['jenkins']['clouds'][i]['docker']['dockerApi']['dockerHost']['uri'])
         docker_cloud_ips.append(re.findall(r"tcp://(.+?):4243",
@@ -85,6 +86,7 @@ def add_new_ec2_instance_log(instance_id, public_ip, launch_time):
 # f.close()
 # 修改yaml配置
 
+# delete ec2 instance history in ec2_creation_history.yaml by instance id
 def delete_ec2_instance_log_by_id(instance_id):
     with open("ec2_creation_history.yaml", "r") as f:
         # print(f.read())
@@ -97,3 +99,33 @@ def delete_ec2_instance_log_by_id(instance_id):
         print(x)
         with open("ec2_creation_history.yaml", 'w') as w_f:
             yaml.dump(x, w_f)
+
+
+# add new docker cloud info in jenkins.yaml in jenkins master
+def add_docker_cloud_in_jenkins(public_ip):
+    jenkins_configuration_path = os.path.join(os.getcwd(), "jenkins.yaml")
+    os.path.isfile(jenkins_configuration_path)
+    yaml_infos = read_yaml(jenkins_configuration_path)
+    clouds_info = yaml_infos['jenkins']['clouds']
+    cloud_template_info = read_yaml(os.path.join(os.getcwd(), "docker_cloud.yaml"))
+    cloud_template_info[0]["docker"]["dockerApi"]["dockerHost"]["uri"] = "tcp://" + public_ip + ":4243"
+    clouds_info.append(cloud_template_info[0])
+    with open('jenkins.yaml', 'w') as f:
+        print(yaml_infos)
+        print(clouds_info)
+        yaml.dump(yaml_infos, f)
+
+
+# delete new docker cloud info in jenkins.yaml in jenkins master
+def delete_docker_cloud_in_jenkins(deleted_public_ip):
+    jenkins_configuration_path = os.path.join(os.getcwd(), "jenkins.yaml")
+    os.path.isfile(jenkins_configuration_path)
+    yaml_infos = read_yaml(jenkins_configuration_path)
+    clouds_info = yaml_infos['jenkins']['clouds']
+    yaml_infos['jenkins']['clouds'] = list(filter(
+        lambda node: (re.findall(r"tcp://(.+?):4243", node['docker']['dockerApi']['dockerHost']["uri"])[0] !=
+                      deleted_public_ip), clouds_info))
+    with open('jenkins.yaml', 'w') as f:
+        print(yaml_infos)
+        print(yaml_infos['jenkins']['clouds'])
+        yaml.dump(yaml_infos, f)
